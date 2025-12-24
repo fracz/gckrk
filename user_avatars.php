fd96cfa86dabb55ea46738be0e60e0781a7ccc43
Wojciech Frącz <fraczwojciech@gmail.com>
2025-12-24 20:27:01 +0100

    Working on users

--- user_avatars.php
+++ user_avatars.php
@@ -2,4 +2,8 @@
 return [
     'kranfagel' => 'https://img.geocaching.com/user/square250/8fc9c649-e009-4f6e-95d9-61dac5bb95ea.jpg',
     'j_janus' => 'https://img.geocaching.com/user/square250/552a5869-4a1a-40cf-9fdf-f2ff8c558a6e.jpg',
+    'EkaSyr_Mantis' => 'https://img.geocaching.com/user/square250/e6e4d768-601b-484c-b6a3-28ef63253d9f.jpg',
+    'seba54' => 'https://img.geocaching.com/user/square250/8f75fa34-8fd0-475d-b8d7-bad1644a74fa.jpg',
+    'm2mPL' => 'https://img.geocaching.com/user/square250/fcedb2f6-27c3-4462-8fab-e26dc4e8ec49.jpg',
+    'GhostDiamond' => 'https://img.geocaching.com/user/square250/e0b1e998-a938-417b-a153-b555c6bf180c.jpg',
 ];
